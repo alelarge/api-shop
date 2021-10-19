17094b9f28560341ebded0c0c58332a259bb7cc9
alelarge <lelarge.armelle@gmail.com>
2021-10-19 21:56:12 +0200

    configure cors

--- app.js
+++ app.js
@@ -53,7 +53,9 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({
+    'error': err.message,
+  });
 });
 
 /*app.listen(port, () => {
