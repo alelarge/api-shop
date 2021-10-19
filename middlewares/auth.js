1e8cb345d3ced2dc0737f9d053c300ef7be9370e
alelarge <lelarge.armelle@gmail.com>
2021-10-19 11:42:19 +0200

    add road protection

---
+++ middlewares/auth.js
@@ -0,0 +1,25 @@
+const jsonwebtoken = require('jsonwebtoken');
+
+
+module.exports = (req, res, next)=>{
+    try{
+        const token = req.headers.authorization.split(' ')[1];
+        const decodeToken = jsonwebtoken.verify(token , process.env.TOKEN_SECRET);
+
+        if(req.body.userId && req.body.userId !== decodeToken.userId){
+            return res.status(401).json({
+                status: 401,
+                message: 'Invalid User ID'
+            })
+        } else {
+            next()
+        }
+    }
+
+    catch(error){
+        return res.status(500).json({
+            status: 500,
+            message: error.message
+        })
+    }
+}
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,16 +1,17 @@
 var express = require('express');
 var ProductController = require('../controllers/ProductController');
+const auth = require('../middlewares/auth');
 var productImageUpload = require('../middlewares/multer.config');
 var router = express.Router();
 
-router.get('/', ProductController.list);
+router.get('/', auth,  ProductController.list);
 
-router.get('/:id' , ProductController.show);
+router.get('/:id' , auth,  ProductController.show);
 
-router.post('/', productImageUpload, ProductController.create);
+router.post('/', productImageUpload, auth,  ProductController.create);
 
-router.put('/:id', productImageUpload,  ProductController.update);
+router.put('/:id', productImageUpload, auth,   ProductController.update);
 
-router.delete('/:id' , ProductController.remove);
+router.delete('/:id' , auth,  ProductController.remove);
 
 module.exports = router;
